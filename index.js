5b23020ebd6a4662e86ae0980b7129f5aa16bd8f
Aayush mittal <ayushmittal844@gmail.com>
2025-04-18 14:37:44 +0530

    t1

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+const router = express.Router();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    const data = [];
+    res.render('home4.ejs', {data});
+});
+
+app.listen(port, () =>{
+    console.log(`Server is running on http://localhost:${port}`);
+})
